d2cfe31d7db200aeb1f8047cc6666e4504f21b53
Fisher <544473461@qq.com>
2018-06-05 15:59:27 +0800

    积分表迁移

---
+++ database/migrations/2018_06_05_020130_create_point_log_tables.php
@@ -0,0 +1,84 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePointLogTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('point_logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('title', 50)->comment('标题');
+            $table->unsignedMediumInteger('staff_sn')->comment('员工编号');
+            $table->char('staff_name', 10)->comment('员工姓名');
+            $table->unsignedTinyInteger('brand_id')->comment('品牌ID');
+            $table->char('brand_name', 10)->comment('品牌名称');
+            $table->unsignedSmallInteger('department_id')->comment('部门ID');
+            $table->char('department_name', 100)->comment('部门名称');
+            $table->char('shop_sn', 10)->comment('店铺代码');
+            $table->char('shop_name', 50)->comment('店铺名称');
+            $table->mediumInteger('point_a')->default(0)->comment('A分变化');
+            $table->mediumInteger('point_b')->default(0)->comment('B分变化');
+            $table->timestamp('changed_at')->comment('积分变化时间');
+            $table->tinyInteger('source_id')->default(0)->comment('积分来源');
+            $table->unsignedInteger('source_foreign_key')->default(0)->comment('来源关联ID');
+            $table->unsignedMediumInteger('first_approver_sn')->comment('初审人编号');
+            $table->char('first_approver_name', 10)->comment('初审人姓名');
+            $table->unsignedMediumInteger('final_approver_sn')->comment('终审人编号');
+            $table->char('final_approver_name', 10)->comment('终审人姓名');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('point_log_sources', function (Blueprint $table) {
+            $table->tinyInteger('id');
+            $table->char('name', 10);
+        });
+
+        Schema::table('personal_point_statistics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedMediumInteger('staff_sn')->comment('员工编号');
+            $table->char('staff_name', 10)->comment('员工姓名');
+            $table->unsignedTinyInteger('brand_id')->comment('品牌ID');
+            $table->char('brand_name', 10)->comment('品牌名称');
+            $table->unsignedSmallInteger('department_id')->comment('部门ID');
+            $table->char('department_name', 100)->comment('部门名称');
+            $table->char('shop_sn', 10)->comment('店铺代码');
+            $table->char('shop_name', 50)->comment('店铺名称');
+            $table->timestamp('date')->comment('年月（其余默认为0）');
+            $table->mediumInteger('point_a')->comment('A分');
+            $table->mediumInteger('point_b_monthly')->comment('当月B分');
+            $table->mediumInteger('point_b_total')->comment('累计B分');
+            $table->timestamps();
+        });
+
+        Schema::table('personal_point_statistic_logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedMediumInteger('staff_sn')->comment('员工编号');
+            $table->mediumInteger('point_a')->comment('A分');
+            $table->mediumInteger('point_b_monthly')->comment('当月B分');
+            $table->mediumInteger('point_b_total')->comment('累计B分');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('point_logs');
+        Schema::dropIfExists('point_log_sources');
+        Schema::dropIfExists('personal_point_statistics');
+        Schema::dropIfExists('personal_point_statistic_logs');
+    }
+}
---
+++ database/seeds/PointLogSourceSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PointLogSourceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('point_log_sources')->insert([
+            ['id' => 0, 'name' => '系统'],
+            ['id' => 1, 'name' => '固定分'],
+            ['id' => 2, 'name' => '奖扣'],
+            ['id' => 3, 'name' => '任务'],
+            ['id' => 4, 'name' => '考勤'],
+            ['id' => 5, 'name' => '日志'],
+        ]);
+    }
+}
