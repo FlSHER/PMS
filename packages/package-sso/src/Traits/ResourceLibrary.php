c10f1654d444ab124511bd49adc8e083c6dfa0cb
28youth <daviezhang@vip.qq.com>
2018-06-11 09:36:53 +0800

    feat: 添加获取数据公共方法

--- packages/package-sso/src/Traits/ResourceLibrary.php
+++ packages/package-sso/src/Traits/ResourceLibrary.php
@@ -10,13 +10,98 @@ trait ResourceLibrary
      * @param $params 非数组为员工主键
      * @return mixed
      */
-	public function getStaff($params)
-	{
-		if (is_array($params)) {
+    public function getStaff($params)
+    {
+        if (is_array($params)) {
 
-			return $this->get('api/staff/list', ['staff' => $params]);
-		}
+            return $this->get('api/staff', $params);
+        }
 
-		return $this->get('api/staff/'.$params);
-	}
+        return $this->get('api/staff/'.$params);
+    }
+
+    /**
+     * 获取部门信息.
+     * 
+     * @author 28youth
+     * @param  $params 非数组为部门主键
+     * @return mixed
+     */
+    public function getDepartmenet($params)
+    {
+        if (is_array($params)) {
+            
+            return $this->get('api/department', $params);
+        }
+
+        return $this->get('api/department/'.$params);
+    }
+
+    /**
+     * 获取品牌信息.
+     * 
+     * @author 28youth
+     * @param  非数组为品牌主键
+     * @return mixed
+     */
+    public function getBrand($params)
+    {
+        if (is_array($params)) {
+            
+            return $this->get('api/brand', $params);
+        }
+
+        return $this->get('api/brand/'.$params);
+    }
+
+    /**
+     * 获取位置信息.
+     * 
+     * @author 28youth
+     * @param  非数组为主键
+     * @return mixed
+     */
+    public function getPosition($params)
+    {
+        if (is_array($params)) {
+            
+            return $this->get('api/position', $params);
+        }
+
+        return $this->get('api/position/'.$params);
+    }
+
+    /**
+     * 获取商品信息.
+     * 
+     * @author 28youth
+     * @param  非数组为商品主键
+     * @return mixed
+     */
+    public function getShop($params)
+    {
+        if (is_array($params)) {
+            
+            return $this->get('api/shop', $params);
+        }
+
+        return $this->get('api/shop/'.$params);
+    }
+
+    /**
+     * 获取用户角色信息.
+     * 
+     * @param  非数组为主键
+     * @return mixed
+     */
+    public function getRole($params)
+    {
+        if (is_array($params)) {
+            
+            return $this->get('api/role', $params);
+        }
+
+        return $this->get('api/role/'.$params);
+    }
+    
 }
