b957657f1539981bface3eb2053d9a75aad2674e
28youth <daviezhang@vip.qq.com>
2018-06-13 11:23:34 +0800

    chore: 修改参与人抄送人表字段

--- database/migrations/2018_06_05_055031_create_event_log_participant_table.php
+++ database/migrations/2018_06_05_055031_create_event_log_participant_table.php
@@ -15,11 +15,11 @@ class CreateEventLogParticipantTable extends Migration
     {
         Schema::create('event_log_participants', function (Blueprint $table) {
             $table->unsignedInteger('event_log_id')->comment('事件日志ID');
-            $table->unsignedMediumInteger('participant_sn')->comment('事件参与人编号');
-            $table->char('participant_name', 10)->comment('事件参与人姓名');
+            $table->unsignedMediumInteger('staff_sn')->comment('事件参与人编号');
+            $table->char('staff_name', 10)->comment('事件参与人姓名');
 
             $table->foreign('event_log_id')->references('id')->on('event_logs');
-            $table->index('participant_sn');
+            $table->index('staff_sn');
         });
     }
 
--- database/migrations/2018_06_05_055837_create_event_log_addressees_table.php
+++ database/migrations/2018_06_05_055837_create_event_log_addressees_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEventCarbonCopyLogTable extends Migration
+class CreateEventLogAddresseesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateEventCarbonCopyLogTable extends Migration
      */
     public function up()
     {
-        Schema::create('event_carbon_copy_logs', function (Blueprint $table) {
+        Schema::create('event_log_addressees', function (Blueprint $table) {
             $table->unsignedInteger('event_log_id')->comment('事件日志ID');
-            $table->unsignedMediumInteger('addressee_sn')->comment('抄送人人编号');
-            $table->char('addressee_name', 10)->comment('抄送人人姓名');
+            $table->unsignedMediumInteger('staff_sn')->comment('抄送人人编号');
+            $table->char('staff_name', 10)->comment('抄送人人姓名');
 
             $table->foreign('event_log_id')->references('id')->on('event_logs');
-            $table->index('addressee_sn');
+            $table->index('staff_sn');
         });
     }
 
@@ -30,6 +30,6 @@ class CreateEventCarbonCopyLogTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('event_carbon_copy_logs');
+        Schema::dropIfExists('event_log_addressees');
     }
 }
