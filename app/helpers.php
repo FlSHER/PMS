6816f42435ee3e5762c38787aa35ddbd0f64de29
28youth <daviezhang@vip.qq.com>
2018-06-14 15:43:46 +0800

    feat: 添加公共函数定义文件

--- app/helpers.php
+++ app/helpers.php
@@ -2,3 +2,19 @@
 
 namespace App;
 
+use Carbon\Carbon;
+
+/**
+ * 获取本月开始到结束时间.
+ * 
+ * @author 28youth
+ * @return array
+ */
+function curMonthBetween()
+{
+	$time = Carbon::now();
+    $stime = Carbon::create(null, null, 01);
+    $etime = Carbon::create(null, null, $time->daysInMonth);
+
+    return [$stime, $etime];
+}
