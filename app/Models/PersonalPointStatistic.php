de78b8af5e8ccf52d364f58b861aac001920f2e6
28youth <daviezhang@vip.qq.com>
2018-06-20 17:03:14 +0800

    feat: 添加任务日志模型

--- app/Models/PersonalPointStatistic.php
+++ app/Models/PersonalPointStatistic.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class PersonalPointStatistic extends Model
 {
-    //
+	protected $casts = [
+        'source_b_total' => 'array',
+        'source_b_monthly' => 'array'
+    ];
+
+    protected $fillable = [
+        'point_a',
+        'point_b',
+        'shop_sn',
+        'shop_name',
+		'staff_sn',
+        'staff_name',
+        'brand_id',
+        'brand_name',
+        'department_id',
+        'department_name'
+    ];
 }
---
+++ database/migrations/2018_06_20_095353_create_artisan_command_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArtisanCommandLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artisan_command_logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('command_sn', 50)->comment('编号');
+            $table->char('title', 50)->comment('标题');
+            $table->string('description')->default('')->comment('说明');
+            $table->string('options')->default('')->comment('选项');
+            $table->unsignedSmallInteger('status')->default(0)->comment('状态 0-未完成 1-已完成');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artisan_command_logs');
+    }
+}
