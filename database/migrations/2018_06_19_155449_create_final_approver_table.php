0bea82a4af1ea30dc61a0e6d2645cf5bb2bbefe2
354226867 <354226867@qq.com>
2018-06-20 10:29:06 +0800

    fix:迁移文件增加字段

--- database/migrations/2018_06_19_155449_create_final_approver_table.php
+++ database/migrations/2018_06_19_155449_create_final_approver_table.php
@@ -17,8 +17,10 @@ class CreateFinalApproverTable extends Migration
             $table->increments('id');
             $table->unsignedMediumInteger('staff_sn')->comment('编号');
             $table->char('staff_name',10)->comment('姓名');
-            $table->mediumInteger('point_a_limit')->comment('A分审核上限');
-            $table->mediumInteger('point_b_limit')->comment('B分审核上限');
+            $table->mediumInteger('point_a_awarding_limit')->comment('加A分审核上限');
+            $table->mediumInteger('point_a_deducting_limit')->comment('减A分审核上限');
+            $table->mediumInteger('point_b_awarding_limit')->comment('加B分审核上限');
+            $table->mediumInteger('point_b_deducting_limit')->comment('减B分审核上限');
             $table->index('staff_sn');
             $table->timestamps();
             $table->softDeletes();
