add6ff65f408d25c3ce326a5e0bf95ca2f524046
354226867 <354226867@qq.com>
2018-06-21 10:21:01 +0800

    feat:终审人服务

---
+++ app/Services/Admin/FinalsService.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Services\Admin;
+
+use App\Repositories\FinalsRepositories;
+use Illuminate\Http\Request;
+
+class FinalsService//todo 验证编号唯一性
+{
+    protected $finals;
+
+    public function __construct(FinalsRepositories $finals)
+    {
+        $this->finals = $finals;
+    }
+
+    public function getFinalsList($request)
+    {
+        return $this->finals->getFinalsAll($request);
+    }
+
+    /**
+     * @param $request
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     * 添加终审人
+     */
+    public function addFinals($request)
+    {
+        if ($this->finals->repetition($request->staff_sn)) {
+            abort(400, '当前员工编号已存在');
+        };
+        $id = $this->finals->delRepetition($request->staff_sn);
+        if (true == (bool)$id) {
+            $this->finals->restoreDeleteFinals($id);
+            return $this->finals->deleteUpdateRestore($id, $request);
+        }
+        $bool = $this->finals->addFinals($request);
+        if (false == (bool)$bool) {
+            abort(404, '数据添加失败');
+        }
+        return response($this->finals->getFinals($bool), 201);
+    }
+
+    /**
+     * @param $request
+     * @return mixed
+     * 编辑终审人
+     */
+    public function updateFinals($request)
+    {
+        if ($this->finals->editRepetition($request->route('id'), $request->staff_sn)) {
+            abort(400, '当前员工编号已存在');
+        }
+        $id = $this->finals->delRepetition($request->staff_sn);
+        if (true == (bool)$id) {
+            $this->finals->restoreDeleteFinals($id);
+            return $this->finals->deleteUpdateRestore($id, $request);
+        }
+        return $this->finals->updateFinals($request);
+    }
+
+    public function deleteFinals($request)
+    {
+        if (false == (bool)$this->finals->deleteFinalsData($request->route('id'))) {
+            return response('删除失败', 404);
+        }
+        return response('', 201);
+    }
+}
