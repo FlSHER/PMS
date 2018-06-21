5ca80f88e4acfa6ce96e6b02d935e7afa9b69b9d
28youth <daviezhang@vip.qq.com>
2018-06-21 10:15:51 +0800

    chore: 重构我的积分统计

--- app/Http/Controllers/APIs/StaffPointController.php
+++ app/Http/Controllers/APIs/StaffPointController.php
@@ -21,39 +21,21 @@ class StaffPointController extends Controller
     public function index(Request $request)
     {
         $user = $request->user();
-        $totalSource = $this->totalSource($user);
         $datetime = $request->query('datetime');
         // 当前月统计
         if (Carbon::parse($datetime)->isCurrentMonth()) {
-
-            $items = $this->currentMonthCredit($user);
+            $monthly = StatisticModel::query()
+                ->where('staff_sn', $user->staff_sn)
+                ->orderBy('calculated_at', 'desc')
+                ->first();
         } else {
             $monthly = StatisticLogModel::query()
-                ->select('source_b_monthly')
                 ->where('staff_sn', $user->staff_sn)
                 ->whereBetween('date', monthBetween($datetime))
                 ->first();
-
-            $source_b_total = collect($monthly['source_b_monthly'])
-                ->reduce(function ($carry, $item) {
-                    return $carry + $item['total_b'];
-                });
-
-            $items = [
-                'point_b_total' => $source_b_total,
-                'source_b_monthly' => $monthly['source_b_monthly']
-            ];
         }
-        $items['source_b_total'] = $totalSource;
 
-        $datas = array_merge($items, [
-            'department' => $user->department,
-            'realname' => $user->realname,
-            'staff_sn' => $user->staff_sn,
-            'brand' => $user->brand
-        ]);
-
-        return response()->json($datas);
+        return response()->json($monthly);
     }
 
     /**
@@ -87,65 +69,5 @@ class StaffPointController extends Controller
         $pointlog->load('source');
 
         return response()->json($pointlog);
-    }
-
-    /**
-     * 统计员工分类积分数.
-     *
-     * @author 28youth
-     * @param  $user
-     * @return array
-     */
-    public function totalSource($user)
-    {
-        // 获取上次月结积分统计
-        $prevMonth = StatisticLogModel::query()
-            ->select('source_b_monthly')
-            ->where('staff_sn', $user->staff_sn)
-            ->orderBy('created_at', 'desc')
-            ->first();
-
-        // 获取本月积分分类统计
-        $curMonth = PointLogModel::where('staff_sn', $user->staff_sn)
-            ->select('source_id', \DB::raw('SUM(point_b) as total_b'))
-            ->whereBetween('created_at', monthBetween())
-            ->groupBy('source_id')
-            ->get();
-
-        // 合并分类积分统计
-        $total = $curMonth->map(function ($item, $key) use ($prevMonth) {
-            $source = $prevMonth['source_b_monthly'];
-            if ($source[$key]['source_id'] === $item['source_id']) {
-                $item['total_b'] += $source[$key]['total_b'];
-            }
-            return $item;
-        });
-
-        return $total;
-    }
-
-    /**
-     * 统计员工当月积分情况.
-     *
-     * @author 28youth
-     * @param  Request $request
-     * @return mixed
-     */
-    protected function currentMonthCredit($user)
-    {
-        $curMonth = PointLogModel::where('staff_sn', $user->staff_sn)
-            ->select('source_id', \DB::raw('SUM(point_b) as total_b'))
-            ->whereBetween('created_at', monthBetween())
-            ->groupBy('source_id')
-            ->get();
-
-        $totalB = $curMonth->reduce(function ($carry, $item) {
-            return $carry + $item['total_b'];
-        });
-
-        return [
-            'source_b_monthly' => $curMonth->toArray(),
-            'point_b_total' => $totalB
-        ];
     }
 }
