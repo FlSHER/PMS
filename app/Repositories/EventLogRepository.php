58c1726bb8f53a2a98f0622c766ba7e6b0cf9329
28youth <daviezhang@vip.qq.com>
2018-06-26 17:00:52 +0800

    chore: 事件列表返回分页信息

--- app/Repositories/EventLogRepository.php
+++ app/Repositories/EventLogRepository.php
@@ -52,7 +52,7 @@ class EventLogRepository
         
         return $this->eventlog->filterByQueryString()
             ->sortByQueryString()
-            ->get();
+            ->withPagination();
     }
 
     /**
