d346972b33a3b9433a8c45f6cc7ba6476a08ee01
28youth <daviezhang@vip.qq.com>
2018-06-27 16:07:48 +0800

    feat: 存储单个配置接口

--- app/Http/Controllers/Admin/BasePointController.php
+++ app/Http/Controllers/Admin/BasePointController.php
@@ -17,10 +17,10 @@ class BasePointController extends Controller
 	 */
 	public function index(Request $request)
 	{
-		$type = $request->query('type', 'education');
+		$name = $request->query('name', 'education');
 
 		$datas = CommonConfig::byNamespace('basepoint')
-			->where('name', sprintf('basepoint:%s', $type))
+			->where('name', $name)
 			->first();
 
 		return response()->json($datas ? json_decode($datas->value) : [], 200);
@@ -37,26 +37,66 @@ class BasePointController extends Controller
 	{
 		$rules = [
             'datas' => 'required|array',
-            'type' => 'required|string|in:type,education,position,speciality,seniority',
+            'name' => 'required|string|in:name,education,position,speciality,seniority',
             'datas.*.point' => 'required|integer|min:0'
         ];
         $messages = [
             'datas.required' => '输入的选项内容不能为空',
-            'type.required' => '输入的选项类型不能为空', 
+            'name.required' => '输入的选项名称不能为空', 
             'datas.*.point.required' => '输入的配置分不能为空',
             'datas.*.point.integer' => '输入的配置分必须为一个数字',
             'datas.*.point.min' => '输入的配置分不能小于0'
         ];
         $this->validate($request, $rules, $messages);
 
-        $type = $request->input('type');
+        $name = $request->input('name');
         $datas = $request->input('datas');
 
         CommonConfig::updateOrCreate(
-            ['namespace' => 'basepoint', 'name' => sprintf('basepoint:%s', $type)],
+            ['namespace' => 'basepoint', 'name' => $name],
             ['value' => json_encode($datas)]
         );
 
         return response()->json(null, 201);
 	}
+
+
+	/**
+     * 配置单个规则内容.
+     *
+	 * @author 28youth
+	 * @param  \Illuminate\Http\Request $request
+	 * @return mixed
+     */
+    public function storeSingle(Request $request)
+    {
+    	$rules = [
+            'name' => 'required|string|in:name,max_point,ratio',
+            'value' => 'required',
+            'datas' => 'nullable|array',
+        ];
+        $messages = [
+            'name.required' => '输入的选项名称不能为空', 
+            'value.required' => '输入的配置值不能为空',
+        ];
+        $this->validate($request, $rules, $messages);
+        
+        $datas = $request->input('datas', '');
+
+        if ($datas) {
+        	collect($datas)->map(function ($data) {
+		        CommonConfig::updateOrCreate(
+		            ['namespace' => 'basepoint', 'name' => $data['name']],
+		            ['value' => $data['value']]
+		        );
+        	});
+        } else {
+	        CommonConfig::updateOrCreate(
+	            ['namespace' => 'basepoint', 'name' => $request->input('name')],
+	            ['value' => $request->input('value')]
+	        );
+        }
+
+        return response()->json(['message' => '操作成功'], 201);
+    }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -54,6 +54,10 @@ Route::group(['middleware' => 'auth:api'], function (RouteContract $admin) {
     // @patch /admin/base-points/setting
     $admin->patch('base-points/setting', Admin\BasePointController::class . '@store');
 
+    // 存储单个配置
+    // @patch /admin/base-points/setting
+    $admin->post('base-points/setting', Admin\BasePointController::class . '@storeSingle');
+
     // route 证书配置
     $admin->group(['prefix' => 'certificates'], function (RouteContract $admin) {
 
