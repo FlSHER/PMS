add829ae05888e47fdaf89ac29ca8ee13d45a76b
28youth <daviezhang@vip.qq.com>
2018-06-28 12:35:16 +0800

    chore: 重构基础分获取接口

--- app/Http/Controllers/Admin/BasePointController.php
+++ app/Http/Controllers/Admin/BasePointController.php
@@ -7,24 +7,66 @@ use App\Models\CommonConfig;
 
 class BasePointController extends Controller
 {
+
+    /**
+     * 获取基础分配置.
+     * 
+     * @author 28youth
+     * @param  \Illuminate\Http\Request $request
+     * @return mixed
+     */
+    public function index(Request $request)
+    {
+        $name = $request->query('name', 'education');
+
+        $items = app()->call([$this, camel_case('get_'.$name)]);
+
+        return response()->json($items, 200);
+    }
 	
 	/**
-	 * 获取基础分配置.
+	 * 获取学历分配置.
 	 * 
 	 * @author 28youth
-	 * @param  \Illuminate\Http\Request $request
-	 * @return mixed
+	 * @return App\Models\CommonConfig
 	 */
-	public function index(Request $request)
+	public function getEducation()
 	{
-		$name = $request->query('name', 'education');
+        $data = CommonConfig::byNamespace('basepoint')
+            ->where('name', 'education')
+            ->first();
 
-		$datas = CommonConfig::byNamespace('basepoint')
-			->where('name', $name)
-			->first();
-
-		return response()->json($datas ? json_decode($datas->value) : [], 200);
+        return $data ? json_decode($data->value) : [];
 	}
+
+    /**
+     * 获取职位分配置.
+     * 
+     * @author 28youth
+     * @return App\Models\CommonConfig
+     */
+    public function getPosition(Request $request)
+    {
+        $data = CommonConfig::byNamespace('basepoint')
+            ->where('name', 'position')
+            ->first();
+
+        return $data ? json_decode($data->value) : [];
+    }
+
+    /**
+     * 获取工龄分配置.
+     * 
+     * @author 28youth
+     * @return App\Models\CommonConfiged
+     */
+    public function getSeniority(Request $request)
+    {
+        return CommonConfig::byNamespace('basepoint')
+            ->select(['name', 'value'])
+            ->whereIn('name', ['max_point', 'ratio'])
+            ->get();
+    }
 
 	/**
 	 * 存储基础分配置.
@@ -36,25 +78,25 @@ class BasePointController extends Controller
 	public function store(Request $request)
 	{
 		$rules = [
-            'datas' => 'required|array',
-            'name' => 'required|string|in:name,education,position,speciality,seniority',
-            'datas.*.point' => 'required|integer|min:0'
+            'data' => 'required|array',
+            'name' => 'required|string|in:name,education,position,seniority',
+            'data.*.point' => 'required|integer|min:0'
         ];
         $messages = [
-            'datas.required' => '输入的选项内容不能为空',
+            'data.required' => '输入的选项内容不能为空',
             'name.required' => '输入的选项名称不能为空', 
-            'datas.*.point.required' => '输入的配置分不能为空',
-            'datas.*.point.integer' => '输入的配置分必须为一个数字',
-            'datas.*.point.min' => '输入的配置分不能小于0'
+            'data.*.point.required' => '输入的配置分不能为空',
+            'data.*.point.integer' => '输入的配置分必须为一个数字',
+            'data.*.point.min' => '输入的配置分不能小于0'
         ];
         $this->validate($request, $rules, $messages);
 
         $name = $request->input('name');
-        $datas = $request->input('datas');
+        $data = $request->input('data');
 
         CommonConfig::updateOrCreate(
             ['namespace' => 'basepoint', 'name' => $name],
-            ['value' => json_encode($datas)]
+            ['value' => json_encode($data)]
         );
 
         return response()->json(null, 201);
@@ -62,40 +104,33 @@ class BasePointController extends Controller
 
 
 	/**
-     * 配置单个规则内容.
+     * 配置工龄上限.
      *
 	 * @author 28youth
 	 * @param  \Illuminate\Http\Request $request
 	 * @return mixed
      */
-    public function storeSingle(Request $request)
+    public function storeSeniority(Request $request)
     {
     	$rules = [
-            'name' => 'required|string|in:name,max_point,ratio',
-            'value' => 'required',
-            'datas' => 'nullable|array',
+            'data' => 'required|array',
+            'data.*.name' => 'required|string',
+            'data.*.value' => 'required'
         ];
         $messages = [
-            'name.required' => '输入的选项名称不能为空', 
-            'value.required' => '输入的配置值不能为空',
+            'data.*.name.required' => '输入的选项名称不能为空', 
+            'data.*.value.required' => '输入的配置值不能为空',
         ];
         $this->validate($request, $rules, $messages);
         
-        $datas = $request->input('datas', '');
+        $data = $request->input('data', '');
 
-        if ($datas) {
-        	collect($datas)->map(function ($data) {
-		        CommonConfig::updateOrCreate(
-		            ['namespace' => 'basepoint', 'name' => $data['name']],
-		            ['value' => $data['value']]
-		        );
-        	});
-        } else {
-	        CommonConfig::updateOrCreate(
-	            ['namespace' => 'basepoint', 'name' => $request->input('name')],
-	            ['value' => $request->input('value')]
-	        );
-        }
+        collect($data)->map(function ($data) {
+            CommonConfig::updateOrCreate(
+                ['namespace' => 'basepoint', 'name' => $data['name']],
+                ['value' => $data['value']]
+            );
+        });
 
         return response()->json(['message' => '操作成功'], 201);
     }
