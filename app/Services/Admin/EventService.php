9e63d72bcbde947a9f6fcb83badb1a6219ea6148
354226867 <354226867@qq.com>
2018-06-29 16:58:09 +0800

    feat:事件增加最大抄送人，最大值验证

--- app/Services/Admin/EventService.php
+++ app/Services/Admin/EventService.php
@@ -8,9 +8,11 @@
 
 namespace App\Services\Admin;
 
+use Excel;
 use App\Models\Event;
 use App\Models\EventType;
-use Excel;
+use App\Models\FinalApprover;
+use App\Repositories\FinalsRepositories;
 use App\Repositories\EventTypeRepository;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
@@ -19,11 +21,17 @@ use SebastianBergmann\Exporter\Exporter;
 
 class EventService
 {
-    public function __construct(EventRepository $eventRepository, EventTypeRepository $eventTypeRepository, \Maatwebsite\Excel\Excel $excel)
+    protected $eventRepository;
+    protected $eventTypeRepository;
+    protected $excel;
+    protected $finalsRepositories;
+
+    public function __construct(EventRepository $eventRepository, EventTypeRepository $eventTypeRepository, \Maatwebsite\Excel\Excel $excel, FinalsRepositories $finalsRepositories)
     {
         $this->eventRepository = $eventRepository;
         $this->eventTypeRepository = $eventTypeRepository;
         $this->excel = $excel;
+        $this->finalsRepositories = $finalsRepositories;
     }
 
     /**
@@ -44,6 +52,7 @@ class EventService
     public function addEvent($request)
     {
         $this->verifyEventType($request->type_id, $request->name);
+        $this->finalValueVerify($request);
         return $this->eventRepository->addEventData($request);
     }
 
@@ -60,9 +69,10 @@ class EventService
         }
         $event = $this->eventRepository->updateGetOnly($request->route('id'), $request->name);
         if (true == (bool)$event) {
-            abort(404, '当前名字已经被使用');
+            abort(422, '当前名字已经被使用');
         }
-        $request->first_approver_locked = $request->first_approver_sn > 0 ? $request->first_approver_sn : 0;//todo bug 审核人为空 锁定应当为0   数据仓库是接收all在这里改不了
+        $this->finalValueVerify($request);
+        $request->first_approver_locked = $request->first_approver_sn > 0 ? $request->first_approver_locked : 0;
         $request->final_approver_locked = $request->final_approver_sn > 0 ? $request->final_approver_locked : 0;
         return $this->eventRepository->updateEventData($request);
     }
@@ -79,7 +89,44 @@ class EventService
         }
         $nameOnly = $this->eventRepository->nameWhereGetData($name);
         if (true == (bool)$nameOnly) {
-            abort(404, '当前名字已经被占用');
+            abort(422, '当前名字已经被占用');
+        }
+    }
+
+    /**
+     * @param $request
+     * 分值检测不大于终审人
+     */
+    protected function finalValueVerify($request)
+    {
+        if (count($request->default_cc_addressees) > 5) {
+            abort(422, '抄送人不能超过5位');
+        }
+        if ($request->final_approver_sn > 0) {
+            $final = $this->finalsRepositories->repetition($request->final_approver_sn);
+            if (null == $final) {
+                abort(404, '未找到当前终审人');
+            }
+            if (strstr($request['point_a_max'], '-')) {
+                $pointAMax = str_replace('-', ' ', $request['point_a_max']);
+                if ($pointAMax > $final['point_a_deducting_limit']) {
+                    abort(422, 'A分最大扣分值超过终审人上限');
+                }
+            } else {
+                if ($request['point_a_max'] > $final['point_a_awarding_limit']) {
+                    abort(422, 'A分最大加分值超过终审人上限');
+                }
+            }
+            if (strstr($request['point_b_max'], '-')) {
+                $pointBMax = str_replace('-', ' ', $request['point_b_max']);
+                if ($pointBMax > $final['point_b_deducting_limit']) {
+                    abort(422, 'B分最大扣分值超过终审人上限');
+                }
+            } else {
+                if ($request['point_b_max'] > $final['point_b_deducting_limit']) {
+                    abort(422, 'B分最大加分值超过终审人上限');
+                }
+            }
         }
     }
 
@@ -114,7 +161,7 @@ class EventService
      */
     public function excelImport()
     {
-        $str=explode('.',$_FILES['file']['name'])[0];
+        $str = explode('.', $_FILES['file']['name'])[0];
         $excelPath = $_FILES['file']['tmp_name'];
         $res = [];
         Excel::load($excelPath, function ($matter) use (&$res) {
@@ -159,11 +206,35 @@ class EventService
                 }
             }
             if ($res[$i][9] != '') {
-                try {
-                    $finalOa = app('api')->withRealException()->getStaff((int)$res[$i][9]);//todo 终审人里面找   待改
-                } catch (\Exception $e) {
-                    $errorInfo[$str] = ['序号：第' . $x . '条信息添加失败，错误：终审人编号错误'];
+                $finalOa = $this->finalsRepositories->repetition((int)$res[$i][9]);
+                if (true != (bool)$finalOa) {
+                    $errorInfo[$str] = ['序号：第' . $x . '条信息添加失败，错误：不存在的终审人'];
                     $dataInfo[] = $res[$i][9];
+                }
+                $final = $this->finalsRepositories->repetition($res[$i][9]);
+                if (strstr($res[$i][3], '-')) {
+                    $pointAMax = str_replace('-', ' ', $res[$i][3]);
+                    if ($pointAMax > $final['point_a_deducting_limit']) {
+                        $errorInfo[$str] = ['序号：第' . $x . '条信息添加失败，错误：A分最大值超过终审人上限'];
+                        $dataInfo[] = $res[$i][9];
+                    }
+                } else {
+                    if ($res[$i][3] > $final['point_a_awarding_limit']) {
+                        $errorInfo[$str] = ['序号：第' . $x . '条信息添加失败，错误：A分最大值超过终审人上限'];
+                        $dataInfo[] = $res[$i][9];
+                    }
+                }
+                if (strstr($res[$i][5], '-')) {
+                    $pointAMax = str_replace('-', ' ', $res[$i][5]);
+                    if ($pointAMax > $final['point_a_deducting_limit']) {
+                        $errorInfo[$str] = ['序号：第' . $x . '条信息添加失败，错误：B分最大值超过终审人上限'];
+                        $dataInfo[] = $res[$i][9];
+                    }
+                } else {
+                    if ($res[$i][5] > $final['point_a_awarding_limit']) {
+                        $errorInfo[$str] = ['序号：第' . $x . '条信息添加失败，错误：B分最大值超过终审人上限'];
+                        $dataInfo[] = $res[$i][9];
+                    }
                 }
             }
             if ($res[$i][2] >= $res[$i][3]) {
@@ -180,8 +251,13 @@ class EventService
             }
             if (isset($res[$i][12])) {
                 $arr = explode(',', $res[$i][12]);
+                if (count($arr) > 5) {
+                    $errorInfo[$str] = ['序号：第' . $x . '条信息添加失败，错误：抄送人不能超过5人'];
+                    $dataInfo[] = $res[$i][12];
+                }
                 $t = 0;
                 $errorInfo1 = [];
+                $cc=[];
                 foreach ($arr as $k => $v) {
                     $t++;
                     $array = explode('=', $v);
@@ -193,6 +269,10 @@ class EventService
                     if ($oaData['realname'] != $array[1]) {
                         $errorInfo1[] = '抄送人第' . $t . '个编号和名字不匹配';
                     }
+                    $cc[] = [
+                        'staff_sn' => $array[0],
+                        'staff_name' => $array[1],
+                    ];
                 }
                 if ($errorInfo1 != []) {
                     $errorInfo[$str] = ['序号：第' . $x . '条信息添加失败，错误：' . implode('、', $errorInfo1)];
@@ -201,7 +281,7 @@ class EventService
             }
             if ($errorInfo != []) {
                 $err[] = [
-                    'data' => (object)[$str=>implode('、',$res[$i])] ,//$dataInfo,
+                    'data' => (object)[$str => implode('、', $res[$i])],//$dataInfo,
                     'message' => (object)$errorInfo
                 ];
                 continue;
@@ -216,20 +296,20 @@ class EventService
             $model->point_a_default = $res[$i][6];//A分默认
             $model->point_b_default = $res[$i][7];//B分默认
             $model->first_approver_sn = $res[$i][8];//初审编号
-            $model->first_approver_name = $firstOa->realname == true ? $firstOa->realname : "";//初审姓名
+            $model->first_approver_name = $firstOa['realname'] == true ? $firstOa['realname'] : "";//初审姓名
             $model->final_approver_sn = $res[$i][9];//终审编号
-            $model->final_approver_name = $firstOa->realname == true ? $finalOa->realname : "";//终审姓名   //todo  终审人去列表查
+            $model->final_approver_name = isset($finalOa) ? $finalOa->realname == true ? $finalOa->realname : "" : "";//终审姓名
             $model->first_approver_locked = $res[$i][8] == true ? $res[$i][10] : 0;//初审锁定
             $model->final_approver_locked = $res[$i][9] == true ? $res[$i][11] : 0;//终审锁定
-            $model->default_cc_addressees = isset($arr) ? $arr : '';//默认抄送
+            $model->default_cc_addressees = isset($arr) ? $cc : '';//默认抄送
             $model->is_active = $res[$i][13] == 1 ? 1 : 0;//激活
             $success = $model->save();
             if ($success == true) {
-                $successInfo[]=$success->find($success->id);
+                $successInfo[] = $model->find($model->id);
 //                $successInfo[] = $res[$i] . '导入成功';
             }
         }
-        $data['data'] = isset($successInfo) ? (object)$successInfo : [];
+        $data['data'] = isset($successInfo) ? $successInfo : [];
         $data['errors'] = isset($err) ? $err : [];
         return $info[] = $data;
     }
