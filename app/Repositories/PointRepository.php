00f2ac421934a2010f4811bab7b219df063e4082
354226867 <354226867@qq.com>
2018-07-04 14:31:04 +0800

    fix:列表获取方式修改

--- app/Repositories/PointRepository.php
+++ app/Repositories/PointRepository.php
@@ -24,28 +24,7 @@ class PointRepository
      */
     public function getPointList($request)
     {
-        $builder = ($this->point instanceof Model) ? $this->point->query() : $this->point;
-        $sort = explode('-', $request->sort);
-        $limit = $request->query('limit', 20);
-        $filters = $request->query('filters', '');
-        if ($filters && $filters !== null) {
-            $maps = $this->formatFilter($filters);
-            foreach ($maps['maps'] as $k => $map) {
-                $curKey = $maps['fields'][$k];
-                $builder->when($curKey, $map[$curKey]);
-            }
-        }
-        $builder->when(($sort && !$sort), function ($query) use ($sort) {
-            $query->orderBy($sort[0], $sort[1]);
-        });
-        $items = $builder->paginate($limit);
-        return array(
-            'data' => $items->items(),
-            'total' => $items->count(),
-            'page' => $items->currentPage(),
-            'pagesize' => $limit,
-            'totalpage' => $items->total(),
-        );
+        return $this->point->filterByQueryString()->withPagination($request->get('pagesize', 10));
     }
 
     /**
