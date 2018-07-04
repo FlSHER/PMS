556bc8dc026f678c25bd725c205c6613e2a42147
Fisher <544473461@qq.com>
2018-07-04 10:06:44 +0800

    ref:OAuth验证的客户端授权使用的参数改为.env配置

--- packages/package-sso/src/Services/RequestSSOService.php
+++ packages/package-sso/src/Services/RequestSSOService.php
@@ -8,83 +8,83 @@ use Fisher\SSO\Traits\ResourceLibrary;
 
 class RequestSSOService
 {
-	use UserHelper;
-	use ResourceLibrary;
+    use UserHelper;
+    use ResourceLibrary;
 
-	public function __construct(Request $request)
-	{
-		$this->setHeader([
-			'Accept' => 'application/json',
-			'Authorization' => $request->header('Authorization')
-		]);
-	}
+    public function __construct(Request $request)
+    {
+        $this->setHeader([
+            'Accept' => 'application/json',
+            'Authorization' => $request->header('Authorization')
+        ]);
+    }
 
-	/**
-	 * oauth 客户端授权.
-	 * 
-	 * @author 28youth
-	 * @return RequestSSOService
-	 */
-	public function client()
-	{
-		$response = $this->request('post', '/oauth/token', [
-			'headers' => ['Accept' => 'application/json'],
-			'form_params' => [
-		        'grant_type' => 'client_credentials',
-		        'client_id' => 7,
-		        'client_secret' => 'lcBIS0l1eW038wqUgDf6qsNyUl3L69Ck8YxHXKGh',
-		    ]
-		]);
-		$this->setHeader([
-			'Accept' => 'application/json',
-			'Authorization' => 'Bearer '.$response['access_token']
-		]);
+    /**
+     * oauth 客户端授权.
+     *
+     * @author 28youth
+     * @return RequestSSOService
+     */
+    public function client()
+    {
+        $response = $this->request('post', '/oauth/token', [
+            'headers' => ['Accept' => 'application/json'],
+            'form_params' => [
+                'grant_type' => 'client_credentials',
+                'client_id' => config('sso.client_id'),
+                'client_secret' => config('sso.client_secret'),
+            ]
+        ]);
+        $this->setHeader([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $response['access_token']
+        ]);
 
-		return $this;
-	}
+        return $this;
+    }
 
-	protected function getBaseUri(): string
+    protected function getBaseUri(): string
     {
         return config('sso.host');
     }
 
-	public function get($endpoint, $query = [], $header = [])
-	{
-		return $this->request('get', $endpoint, [
+    public function get($endpoint, $query = [], $header = [])
+    {
+        return $this->request('get', $endpoint, [
             'headers' => array_merge($header, $this->headers),
             'query' => $query,
         ]);
-	}
+    }
 
-	public function post($endpoint, $params = [], $header = [])
-	{
-		return $this->request('post', $endpoint, [
+    public function post($endpoint, $params = [], $header = [])
+    {
+        return $this->request('post', $endpoint, [
             'headers' => array_merge($header, $this->headers),
             'json' => $params,
         ]);
-	}
+    }
 
-	public function put($endpoint, $params = [], $header = [])
-	{
-		return $this->request('put', $endpoint, [
+    public function put($endpoint, $params = [], $header = [])
+    {
+        return $this->request('put', $endpoint, [
             'headers' => array_merge($header, $this->headers),
             'json' => $params,
         ]);
-	}
+    }
 
-	public function patch($endpoint, $params = [], $header = [])
-	{
-		return $this->request('patch', $endpoint, [
+    public function patch($endpoint, $params = [], $header = [])
+    {
+        return $this->request('patch', $endpoint, [
             'headers' => array_merge($header, $this->headers),
             'json' => $params,
         ]);
-	}
+    }
 
-	public function delete($endpoint, $params = [], $header = [])
-	{
-		return $this->request('delete', $endpoint, [
+    public function delete($endpoint, $params = [], $header = [])
+    {
+        return $this->request('delete', $endpoint, [
             'headers' => array_merge($header, $this->headers),
             'json' => $params,
         ]);
-	}
+    }
 }
