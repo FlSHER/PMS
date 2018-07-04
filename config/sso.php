8751004e1e6df6cc83fa885fab994b47e9fcf57d
Fisher <544473461@qq.com>
2018-07-04 10:07:49 +0800

    ref:更新sso配置文件

--- config/sso.php
+++ config/sso.php
@@ -3,5 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'host' => env('OA_HOST', 'http://of.xigemall.com'),
+    'host' => env('OA_HOST', ''),
+    'client_id' => env('CLIENT_ID', 0),
+    'client_secret' => env('CLIENT_SECRET', ''),
 ];
