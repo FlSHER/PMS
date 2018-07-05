81f5eade2e77d2aa305c8e496ae8daa05ea18e7e
354226867 <354226867@qq.com>
2018-07-05 09:47:19 +0800

    fix:修改编辑名称重复不通过

--- app/Http/Controllers/Admin/AuthorityController.php
+++ app/Http/Controllers/Admin/AuthorityController.php
@@ -42,7 +42,7 @@ class AuthorityController extends Controller
      */
     public function editGroup(Request $request)//编辑分组
     {
-        $this->addAuthGroupVerify($request);
+        $this->editAuthGroupVerify($request);
         return $this->auth->editAuthGroup($request);
     }
 
@@ -54,12 +54,42 @@ class AuthorityController extends Controller
         return $this->auth->deleteAuthGroup($request);
     }
 
+    /**
+     * 权限分组添加form验证
+     * @param $request
+     */
     public function addAuthGroupVerify($request)
     {
         $this->validate($request, [
             'name' => [
                 'required',
                 Rule::unique('authority_groups', 'name')
+                    ->ignore('id', $request->get('id', 0))
+            ],
+            'departments.*.department_id' => 'numeric',
+            'departments.*.department_name' => '',
+            'staff.*.staff_sn' => 'numeric',
+            'staff.*.staff_name' => '',
+        ], [], [
+            'name' => '分组名称',
+            'departments.*.department_id' => '部门id',
+            'departments.*.department_name' => '部门名称',
+            'staff.*.staff_sn' => '员工编号',
+            'staff.*.staff_name' => '员工姓名',
+        ]);
+    }
+
+    /**
+     * 权限分组编辑form验证
+     * @param $request
+     */
+    public function editAuthGroupVerify($request)
+    {
+        $this->validate($request, [
+            'name' => [
+                'required',
+                Rule::unique('authority_groups', 'name')
+                    ->whereNotIn('id',explode(' ',$request->route('id')))
                     ->ignore('id', $request->get('id', 0))
             ],
             'departments.*.department_id' => 'numeric',
