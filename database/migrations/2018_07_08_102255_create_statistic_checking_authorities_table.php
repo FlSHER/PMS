63ab441285836ca40f0a057a2c36f5daf6b7a647
354226867 <354226867@qq.com>
2018-07-09 18:42:13 +0800

    feat:统计查看权限和任务分配权限迁移文件

---
+++ database/migrations/2018_07_08_102255_create_statistic_checking_authorities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatisticCheckingAuthoritiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *$table->softDeletes();
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statistic_checking_authorities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('group_id')->comment('分组id');
+            $table->unsignedMediumInteger('admin_sn')->comment('统计查看者编号');
+            $table->char('admin_name',10)->comment('统计查看者姓名');
+            $table->foreign('group_id')->references('id')->on('authority_groups');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statistic_checking_authorities');
+    }
+}
