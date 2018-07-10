0e625f4d1af7338dc552f6bfc1a2d6da80078f49
Fisher <544473461@qq.com>
2018-07-10 09:12:41 +0800

    ref:调整任务分配，统计查看权限表迁移

--- database/migrations/2018_06_30_103027_create_task_publishing_authorities_table.php
+++ database/migrations/2018_06_30_103027_create_task_publishing_authorities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAuthorityTaskPublishTable extends Migration
+class CreateTaskPublishingAuthoritiesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,12 +14,12 @@ class CreateAuthorityTaskPublishTable extends Migration
     public function up()
     {
         Schema::create('task_publishing_authorities', function (Blueprint $table) {
-            $table->increments('id');
             $table->unsignedInteger('group_id')->comment('分组id');
             $table->unsignedMediumInteger('admin_sn')->comment('管理者编号');
-            $table->char('admin_name',10)->comment('管理者姓名');
+            $table->char('admin_name', 10)->comment('管理者姓名');
             $table->timestamps();
             $table->foreign('group_id')->references('id')->on('authority_groups');
+            $table->index(['group_id', 'admin_sn']);
         });
     }
 
--- database/migrations/2018_07_08_102255_create_statistic_checking_authorities_table.php
+++ database/migrations/2018_07_08_102255_create_statistic_checking_authorities_table.php
@@ -14,12 +14,12 @@ class CreateStatisticCheckingAuthoritiesTable extends Migration
     public function up()
     {
         Schema::create('statistic_checking_authorities', function (Blueprint $table) {
-            $table->increments('id');
             $table->unsignedInteger('group_id')->comment('分组id');
             $table->unsignedMediumInteger('admin_sn')->comment('统计查看者编号');
-            $table->char('admin_name',10)->comment('统计查看者姓名');
-            $table->foreign('group_id')->references('id')->on('authority_groups');
+            $table->char('admin_name', 10)->comment('统计查看者姓名');
             $table->timestamps();
+            $table->foreign('group_id')->references('id')->on('authority_groups');
+            $table->index(['group_id', 'admin_sn']);
         });
     }
 
