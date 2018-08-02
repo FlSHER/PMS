ffe8fcaf318c18aa7a0e8181cd16ad661e0b4050
28youth <daviezhang@vip.qq.com>
2018-08-02 11:32:27 +0800

    chore: 修改事件抄送人字段

--- database/migrations/2018_06_05_055837_create_event_log_addressees_table.php
+++ database/migrations/2018_06_05_055837_create_event_log_addressees_table.php
@@ -14,11 +14,11 @@ class CreateEventLogAddresseesTable extends Migration
     public function up()
     {
         Schema::create('event_log_addressees', function (Blueprint $table) {
-            $table->unsignedInteger('event_log_id')->comment('事件日志ID');
+            $table->unsignedInteger('event_log_group_id')->comment('事件日志ID');
             $table->unsignedMediumInteger('staff_sn')->comment('抄送人人编号');
             $table->char('staff_name', 10)->comment('抄送人人姓名');
 
-            $table->foreign('event_log_id')->references('id')->on('event_log_groups');
+            $table->foreign('event_log_group_id')->references('id')->on('event_log_groups');
             $table->index('staff_sn');
         });
     }
